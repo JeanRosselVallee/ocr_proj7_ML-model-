a57ed43adeb29670199a36f9eacffa4934905f60
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-21 10:09:29 +0000

    Updated test file to test prediction by model API

--- tests/unit_test_2.py
+++ tests/unit_test_2.py
@@ -6,6 +6,6 @@ def result():
     req_post = requests.post(   url     = 'http://localhost:5678/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = json.dumps({'inputs': dict_features}) )
-    dict_prediction = json.loads(req_post.text)     #{"predictions":[1]}
+    dict_prediction = json.loads(req_post.text)
     assert dict_prediction == {"predictions":[1]}
 
