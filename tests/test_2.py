6add74704aaf62c11a1daddfcf3520b855d2316f
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-21 10:49:50 +0000

    Updated test file with printed message

--- tests/test_2.py
+++ tests/test_2.py
@@ -2,10 +2,22 @@ import requests
 import json
 
 def test_prediction():    
+
+    # Inputs
     dict_features = {"umap_x":[".3"],"umap_y":[".3"]}
+    print("input features:", dict_features)
+
+    # Send input data to prediction API
     req_post = requests.post(   url     = 'http://13.92.86.145:5678/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = json.dumps({'inputs': dict_features}) )
-    dict_prediction = json.loads(req_post.text)
-    assert dict_prediction == {"predictions":[1]}
+
+    # Output
+    dict_expected = {"predictions":[1]}
+
+    # Get predicted value from API
+    dict_predicted = json.loads(req_post.text)
+
+    # Compare predicted & expected values
+    assert dict_predicted == dict_expected
 
