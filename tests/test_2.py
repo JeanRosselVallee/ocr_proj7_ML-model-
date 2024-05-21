543ae01bc192323cc6452ed15494bb7f41105b48
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-21 12:56:16 +0000

    Added for-loop to test multiple tuples

--- tests/test_2.py
+++ tests/test_2.py
@@ -2,26 +2,25 @@ import requests
 import json
 
 def predict(dict_features):    
-
-    print("input features:", dict_features)
-
     # Send input data to prediction API
     req_post = requests.post(   url     = 'http://13.92.86.145:5678/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = json.dumps({'inputs': dict_features}) )
-
-    # Output
-    dict_expected = {"predictions":[1]}
-    print("output expected:", dict_expected)
-
     # Get predicted value from API
     dict_predicted = json.loads(req_post.text)
-    print("output predicted:", dict_predicted)
-
-    # Compare predicted & expected values
-    assert dict_predicted == dict_expected
 
 def test_prediction():    
-    # Inputs
-    dict_features = {"umap_x":[".3"],"umap_y":[".3"]}
-    dict_predicted = predict(dict_features)
+    for in_1, in_2, out_i = [
+            (.2, .2, 1),
+            (.3, .3, 1),
+            (.4, .4, 1) ] :
+        dict_features_i  = {'umap_x':[str(in_A)],'umap_y':[str(in_B)]}
+        dict_predicted_i = predict(dict_features)
+        dict_expected_i  = {'predictions':[out_i]}
+        print('input features  :', dict_features)
+        print('output predicted:', dict_predicted)
+        print('output expected :', dict_expected)
+
+        # Compare predicted & expected values
+        assert dict_predicted == dict_expected
+
