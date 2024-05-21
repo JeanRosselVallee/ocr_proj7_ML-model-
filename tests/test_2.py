511e644b0bfa9f85639c8a1c03eb9c1c234f3fd2
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-21 10:31:40 +0000

    Replaced localhost by actual server IP

--- tests/test_2.py
+++ tests/test_2.py
@@ -3,7 +3,7 @@ import json
 
 def test_prediction():    
     dict_features = {"umap_x":[".3"],"umap_y":[".3"]}
-    req_post = requests.post(   url     = 'http://localhost:5678/invocations', 
+    req_post = requests.post(   url     = 'http://http://13.92.86.145:5678/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = json.dumps({'inputs': dict_features}) )
     dict_prediction = json.loads(req_post.text)
