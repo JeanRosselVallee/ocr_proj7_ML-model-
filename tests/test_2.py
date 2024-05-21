eb0141a62d64e24a79d64b93aab902196a8bc68c
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-21 13:25:32 +0000

    Added test N°

--- tests/test_2.py
+++ tests/test_2.py
@@ -11,14 +11,15 @@ def predict(dict_features):
     return dict_predicted
 
 def test_prediction():    
-    for in_1, in_2, out_i in [
+    for idx, (in_1, in_2, out_i) in enumerate([
                         (.2, .2, 1),
                         (.3, .3, 1),
-                        (.4, .4, 0) ] :
+                        (.4, .4, 0)   ]) :
+        print('_____________Unit Test N°', idx + 1, '_______________')
         dict_features_i  = {'umap_x':[str(in_1)],'umap_y':[str(in_2)]}
         dict_predicted_i = predict(dict_features_i)
         dict_expected_i  = {'predictions':[out_i]}
-        print('input features  :', dict_features_i)
+        print('input features   :', dict_features_i)
         print('output predicted:', dict_predicted_i)
         print('output expected :', dict_expected_i)
 
